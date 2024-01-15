2b59efa06b58992d357f42c0a4b2eb7abe77e39a
Julie Shapovalenko <jools.shapo@gmail.com>
2024-01-16 00:33:15 +0200

    add dashboard and customers section css

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const sidebar = document.querySelector(".sidebar-js");
+const sidebarNav = document.querySelector(".sidebar__nav");
+const sidebarBtnOpen = document.querySelector(".dashboard__sidebar--mob-open");
+
+const handleSidebarClose = () => {
+  sidebar.style.display = "none";
+  sidebarBtnOpen.style.display = "block";
+};
+
+const handleSidebarOpen = () => {
+  sidebar.style.display = "block";
+  sidebarBtnOpen.style.display = "none";
+};
+
+sidebarNav.addEventListener("click", handleSidebarClose);
+sidebarBtnOpen.addEventListener("click", handleSidebarOpen);
