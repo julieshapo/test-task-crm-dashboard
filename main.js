b8af3ec1e7e17f3d946835b24bf996eb422630e9
Julie Shapovalenko <jools.shapo@gmail.com>
2024-01-16 11:35:31 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 const sidebar = document.querySelector(".sidebar-js");
 const sidebarNav = document.querySelector(".sidebar__nav");
 const sidebarBtnOpen = document.querySelector(".dashboard__sidebar--mob-open");
